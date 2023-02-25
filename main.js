822b554c75420b968a0f192c12bb2b4f212f939a
Yutsuki Zaitsu <89077321+YuiZaitsu@users.noreply.github.com>
2023-02-25 15:04:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-song1="";
-song2="";
+song="";
 leftwristx=0;
 leftwristy=0;
 rightwristx=0;
 rightwristy=0;
+score_leftwrist=0;
+score_rightwrist=0;
+
+
 function preload(){
-song1=loadSound("music.mp3");
-song2=loadSound("music2.mp3");
+song=loadSound("music.mp3");
+
 }
 
 function play(){
-    song1.play();
+    song.play();
+    song.setVolume(0.7);
+    song.rate(1);
+
     
 }
 
@@ -22,11 +28,48 @@ video.hide();
 posenet=ml5.poseNet(video,modelloaded);
 posenet.on("pose",gotPoses);
 
-
 }
 
 function draw(){
 image(video,0,0,600,500);
+fill("red");
+stroke("red");
+if(score_leftwrist>0.2){
+
+circle(leftwristx,leftwristy,20);
+in_number_leftwristy=Number(leftwristy);
+remove_decimal=floor(in_number_leftwristy);
+volume=remove_decimal/500;
+document.getElementById("volume").innerHTML="Volume= "+volume;
+song.setVolume(volume);
+
+
+}
+if(score_rightwrist>0.2){
+circle(rightwristx,rightwristy,20);
+if(rightwristy>0&&rightwristy<=100){
+document.getElementById("speed").innerHTML="speed=0.5x";
+song.rate(0.5);
+
+}
+else if(rightwristy>100&&rightwristy<=200){
+    document.getElementById("speed").innerHTML="speed=1x";
+    song.rate(1);
+}
+else if(rightwristy>200&&rightwristy<=300){
+    document.getElementById("speed").innerHTML="speed=1.5x";
+    song.rate(1.5);
+}
+else if(rightwristy>300&&rightwristy<=400){
+    document.getElementById("speed").innerHTML="speed=2x";
+    song.rate(2);
+}
+else if(rightwristy>400&&rightwristy<=500){
+    document.getElementById("speed").innerHTML="speed=2.5x";
+    song.rate(2.5);
+}
+}
+
 }
 
 function modelloaded(){
@@ -46,5 +89,10 @@ if(results.length>0)
     rightwristy=results[0].pose.rightWrist.y;
     console.log("rightwristx="+rightwristx);
     console.log("rightwristy="+rightwristy);
+    score_leftwrist=results[0].pose.keypoints[9].score;
+    console.log("sscore_leftwrist="+score_leftwrist);
+    score_rightwrist=results[0].pose.keypoints[10].score;
+    console.log("sscore_rightwrist="+score_rightwrist);
 }
 }
+
